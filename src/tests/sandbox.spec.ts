625783e5636a23d6518b978a5542d4f14fd540d8
Joel Sullivan <jsullivan@morsecorp.com>
2021-03-02 14:11:35 -0500

    Migrate to eslint

--- src/tests/sandbox.spec.ts
+++ src/tests/sandbox.spec.ts
@@ -1,5 +1,3 @@
-import { chromium, Browser, Page } from "playwright-chromium";
-
 describe("Sandbox", () => {
   test("should be on the sandbox", async () => {
     await page.goto("https://e2e-boilerplate.github.io/sandbox/", {
@@ -8,7 +6,7 @@ describe("Sandbox", () => {
     await page.waitForSelector("h1");
     const title = await page.$eval(
       "h1",
-      (el: { textContent: any }) => el.textContent
+      (el: { textContent: unknown }) => el.textContent
     );
 
     expect(await page.title()).toEqual("Sandbox");
